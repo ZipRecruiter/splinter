683aa777c74c345d4c1a184c1273eaa5808b9562
Arthur Axel 'fREW' Schmidt <git@frew.co>
2020-01-18 09:31:42 -0800

    Add more tests

--- pairs/interface_test.go
+++ pairs/interface_test.go
@@ -11,19 +11,25 @@ func TestFuncOffset(t *testing.T) {
 	type Test struct {
 		in  string
 		out funcOffset
+		err string
 	}
 
 	tests := []Test{
-		{".Log=0", funcOffset{funcSelector{fun: "Log"}: 0}},
-		{"go.zr.org/common/go/errors/details.Pairs.AddPairs=1", funcOffset{funcSelector{pkg: "go.zr.org/common/go/errors/details", typ: "Pairs", fun: "AddPairs"}: 1}},
-		{"go.zr.org/common/go/errors.Wrap=2", funcOffset{funcSelector{pkg: "go.zr.org/common/go/errors", fun: "Wrap"}: 2}},
+		{".Log=0", funcOffset{funcSelector{fun: "Log"}: 0}, ""},
+		{"go.zr.org/common/go/errors/details.Pairs.AddPairs=1", funcOffset{funcSelector{pkg: "go.zr.org/common/go/errors/details", typ: "Pairs", fun: "AddPairs"}: 1}, ""},
+		{"go.zr.org/common/go/errors.Wrap=2", funcOffset{funcSelector{pkg: "go.zr.org/common/go/errors", fun: "Wrap"}: 2}, ""},
+		{"wrong", nil, "invalid func offset; should be of form [pkg[.type]].<func>=<offset>"},
+		{".wrong=999999999999999999999999999999999999", nil, `strconv.Atoi: parsing "999999999999999999999999999999999999": value out of range`},
 	}
 
 	for i, test := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			f := funcOffset{}
 			if err := f.Set(test.in); err != nil {
-				panic(err)
+				if d := cmp.Diff(test.err, err.Error()); d != "" {
+					t.Errorf("unexpected error (-expected +got):\n%s", d)
+				}
+				return
 			}
 			if d := cmp.Diff(test.out, f); d != "" {
 				t.Errorf("unexpected result (-expected +got):\n%s", d)
